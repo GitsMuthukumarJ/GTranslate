979184f929a899e2b982488edfc8395544b533d9
Muthukumar J <45478465+GitsMuthukumarJ@users.noreply.github.com>
2022-10-13 16:07:16 +0530

    Update gtranslate_in_php.php

--- gtranslate_in_php.php
+++ gtranslate_in_php.php
@@ -2,6 +2,10 @@
 				.goog-te-banner-frame {
 					display: none;
 				}
+
+				.container1 {
+					margin-top: -14%;
+				}
 			</style>
 			<div id="google_translate_element"></div>
 			<script src="https://translate.google.com/translate_a/element.js?
