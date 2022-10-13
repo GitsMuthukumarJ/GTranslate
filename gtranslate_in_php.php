0dd88e91bea0053972112f376bf4f0d642559aea
Muthukumar J <45478465+GitsMuthukumarJ@users.noreply.github.com>
2022-10-13 18:24:32 +0530

    Update gtranslate_in_php.php

--- gtranslate_in_php.php
+++ gtranslate_in_php.php
@@ -1,21 +1,59 @@
 <style>
-				.goog-te-banner-frame {
-					display: none;
-				}
+								.goog-te-banner-frame {
+									display: none;
+								}
 
-				.container1 {
-					margin-top: -14%;
-				}
-			</style>
-			<div id="google_translate_element"></div>
-			<script src="https://translate.google.com/translate_a/element.js?
+								.container1 {
+									margin-top: -12%;
+								}
+
+								body {
+									top: 0px !important;
+								}
+
+								@media only screen and (min-width: 768px) {
+									.goog-te-gadget {
+										margin-left: 100%;
+									}
+								}
+
+								@media only screen and (max-width: 500px) {
+									.top-bar {
+										display: block;
+									}
+
+									.get-touch {
+										display: none;
+									}
+
+									.goog-te-gadget {
+										margin-left: 60%;
+									}
+								}
+
+								@media only screen and (min-device-width: 0px) and (max-device-width: 325px) {
+									.goog-te-gadget {
+										margin-left: 50%;
+									}
+
+									.container1 {
+										margin-top: 0%;
+									}
+
+									.align-self-center {
+										margin-top: 25%;
+									}
+								}
+							</style>
+							<div id="google_translate_element"></div>
+							<script src="https://translate.google.com/translate_a/element.js?
 cb=googleTranslateElementInit"></script>
-			<script>
-				function googleTranslateElementInit() {
-					new google.translate.TranslateElement({
-							pageLanguage: 'en'
-						},
-						'google_translate_element'
-					);
-				}
-			</script>
+							<script>
+								function googleTranslateElementInit() {
+									new google.translate.TranslateElement({
+											pageLanguage: 'en'
+										},
+										'google_translate_element'
+									);
+								}
+							</script>
